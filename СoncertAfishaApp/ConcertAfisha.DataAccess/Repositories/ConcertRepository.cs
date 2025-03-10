f31e87b1b5395adbb0ec70c744303fbb9c51878c
Kolyadko <artem.7ko52@gmail.com>
2025-03-10 12:36:56 +0300

    Concert Repository

---
+++ СoncertAfishaApp/ConcertAfisha.DataAccess/Repositories/ConcertRepository.cs
@@ -0,0 +1,94 @@
+using ConcertAfisha.Core.Abstractions;
+using ConcertAfisha.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConcertAfisha.DataAccess.Repositories;
+
+public class ConcertRepository(ConcertAfishaAppDBContext dbContext)
+{
+    public async Task<List<Concert>> GetAllAsync()
+    {
+        var concertEntities = await dbContext.ConcertEntities
+            .AsNoTracking()
+            .Include(e => e.Members)
+            .OrderBy(e => e.Date)
+            .ToListAsync();
+        return concertEntities;
+    }
+
+    public async Task<Concert> GetByIdAsync(Guid id)
+    {
+        var concert = await dbContext.ConcertEntities
+            .AsNoTracking()
+            .Include(e => e.Members)
+            .Where(c => c.Id == id)
+            .FirstOrDefaultAsync();
+        return concert;
+    }
+    
+    public async Task<(List<Concert>, int)> GetBySpecificationAsync(ISpecification<Concert> spec, int? page, int? size)
+    {
+        var query = dbContext.ConcertEntities.AsQueryable();
+
+        if (spec.Criteria != null)
+        {
+            query = query.Where(spec.Criteria);
+        }
+
+        if (spec.OrderBy != null)
+        {
+            query = spec.OrderBy(query);
+        }
+
+        var countOfEvents = await query.CountAsync();
+
+        if (page.HasValue && size.HasValue)
+        {
+            query = query.Skip((int)((page - 1) * size)).Take((int)size);
+        }
+
+        var events = await query
+            .Include(e => e.Members)
+            .AsNoTracking()
+            .ToListAsync();
+
+        return (events, countOfEvents);
+    }
+    
+    public async Task<Guid> CreateAsync(Concert receivedConcert)
+    {
+        await dbContext.ConcertEntities.AddAsync(receivedConcert);
+        return receivedConcert.Id;
+    }
+    
+    public async Task UpdateAsync(Concert receivedConcert)
+    {
+        var foundedEvent = await dbContext.ConcertEntities
+            .AsNoTracking()
+            .FirstOrDefaultAsync(e => e.Id == receivedConcert.Id);
+
+        if (foundedEvent != null)
+        {
+            foundedEvent.Title = receivedConcert.Title;
+            foundedEvent.Description = receivedConcert.Description;
+            foundedEvent.Date = receivedConcert.Date;
+            foundedEvent.LocationId = receivedConcert.LocationId;
+            foundedEvent.Category = receivedConcert.Category;
+            foundedEvent.MaxNumberOfMembers = receivedConcert.MaxNumberOfMembers;
+            foundedEvent.ImageUrl = receivedConcert.ImageUrl;
+
+            dbContext.ConcertEntities.Update(receivedConcert);
+        }
+    }
+    
+    public async Task DeleteAsync(Guid id)
+    {
+        var foundedConcert = await dbContext.ConcertEntities
+            .FirstOrDefaultAsync(e => e.Id == id);
+
+        if (foundedConcert != null)
+        {
+            dbContext.ConcertEntities.Remove(foundedConcert);
+        }
+    }
+}
--- СoncertAfishaApp/СoncertAfisha.App/Program.cs
+++ СoncertAfishaApp/СoncertAfisha.App/Program.cs
@@ -1,44 +1,104 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "EventApp API", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef')",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
+
+
+
+
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy =>
+        policy.RequireRole("Admin"));
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateLifetime = true,
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+        };
+    });
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost3000",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+            
+        });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "EventApp API V1"); });
+
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.UseCookiePolicy(new CookiePolicyOptions
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+    MinimumSameSitePolicy = SameSiteMode.Strict,
+    HttpOnly = HttpOnlyPolicy.Always,
+    Secure = CookieSecurePolicy.Always
+});
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseCors("AllowLocalhost3000");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
